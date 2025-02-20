7d820c4d9411dff1c567e1ae63d1805429f0088a
priyanshu-luhar <69760063+priyanshu-luhar@users.noreply.github.com>
2025-02-20 02:03:20 -0800

    Running application

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,33 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Hello {
+    private JPanel panelMain;
+    private JButton actButton;
+
+    public Hello() {
+        actButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String msg = "Hello World";
+                JOptionPane.showMessageDialog(null, msg);
+            }
+        });
+    }
+
     public static void main(String[] args) {
-        System.out.println("My Hello World.");
+        JFrame myFrame = new JFrame("Project-1 App-1");
+        Hello hello = new Hello();
+
+        myFrame.setSize(800, 400);
+
+        myFrame.setContentPane(hello.panelMain);
+
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        myFrame.pack();
+
+        myFrame.setVisible(true);
     }
 }
