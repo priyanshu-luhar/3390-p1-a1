ff374aadcb7a6fb01c74304be8beeff3cee4e763
priyanshu-luhar <69760063+priyanshu-luhar@users.noreply.github.com>
2025-02-20 02:43:00 -0800

    Changed Colors

--- src/Hello.java
+++ src/Hello.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 public class Hello {
     private JPanel panelMain;
     private JButton actButton;
+    private JTextArea myName;
 
     public Hello() {
         actButton.addActionListener(new ActionListener() {
@@ -18,17 +19,19 @@ public class Hello {
     }
 
     public static void main(String[] args) {
-        JFrame myFrame = new JFrame("Project-1 App-1");
+        JFrame myFrame = new JFrame("Luhar Project-1 App-1");
         Hello hello = new Hello();
         int w = 500;
-        int h = 800;
+        int h = 600;
 
         myFrame.setSize(w, h);
-
+        hello.panelMain.setBackground(Color.GRAY);
+        hello.panelMain.setForeground(Color.lightGray);
         hello.panelMain.setPreferredSize(new Dimension(w, h));
         hello.actButton.setPreferredSize(new Dimension(350, 200));
 
         myFrame.setContentPane(hello.panelMain);
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         myFrame.pack();
 
         myFrame.setVisible(true);
